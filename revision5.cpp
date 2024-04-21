33c0e1c43f6f0820d269364ebc22c16694cd7d56
iheartwifi <wanlanhe@gmail.com>
2024-04-21 10:57:37 -0400

    Add functions

--- revision5.cpp
+++ revision5.cpp
@@ -5,13 +5,11 @@ using namespace std;
 
 int main() {
     ifstream fichier("notes.txt"); // Ouvrir le fichier en mode lecture
-    ofstream fichier("resultats.txt");
+    ofstream fichierSortant("resultats.txt");
     string ligne; // Variable pour stocker chaque ligne lue
 
     if (fichier.is_open()) { // Vérifier si le fichier est ouvert avec succès
-        while (getline(fichier, ligne)) { // Lire chaque ligne du fichier
-            cout << ligne << endl; // Afficher la ligne lue
-        }
+
         fichier.close(); // Fermer le fichier après avoir terminé de le lire
     } else {
         cout << "Impossible d'ouvrir le fichier." << endl;
